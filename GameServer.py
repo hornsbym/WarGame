350eef97162852d520ce6ed79c48c9c2c9f90ab9
Mitch Hornsby <hornsbym19@mail.wlu.edu>
2019-01-31 18:18:48 -0500

    Reworked some print statements to make them more meaningful.

--- GameServer.py
+++ GameServer.py
@@ -34,7 +34,7 @@ class GameServer(Thread):
         # Saves the print statements to a local text file:
         self.filepath = os.getcwd()
         self.logs = open(str(self.filepath)+"/_logs/"+ str(self.PORT) + ".txt", "a+")
-        print("\n\n*****NEW SERVER SESSION*****", file=self.logs)
+        print("\n*****NEW SERVER SESSION*****", file=self.logs)
 
         self.clients = []
         self.clientScreenDimensions = {}
@@ -44,9 +44,8 @@ class GameServer(Thread):
 
         while True:
             try:
-                print("Trying to bind to port", self.PORT, file=self.logs)
                 self.socket.bind((self.HOST,self.PORT))     # Tries to bind socket to port
-                print("UDP Server started at", self.PORT, file=self.logs)
+                print("UDP Server started at ("+self.HOST, ",", self.PORT +")", file=self.logs)
                 break                                       # Has bound to a port, exits the while loop
             except:
                 self.PORT += 1      # If port is taken, tries the next port up
@@ -84,7 +83,7 @@ class GameServer(Thread):
     def lobbyStage(self):
         """Handles connecting players and setting up the game."""
         # Organizes outbound data to clients into a dict
-        print('Starting lobby stage.', file=self.logs)
+        print("("+self.HOST, ",", self.PORT +"):: Starting lobby stage.", file=self.logs)
         gameState = {
             "connection": str(self.PORT), 
             "ready":False,
@@ -94,10 +93,6 @@ class GameServer(Thread):
             }
 
         while True:
-            # Continuously saves logging information to a text file:
-            self.logs.close()
-            self.logs = open(str(self.filepath)+"/_logs/"+ str(self.PORT) + ".txt", "a+")
-
             inboundData = self.socket.recvfrom(1024)      # Gets bundle of data from clients
             data = inboundData[0]                         # Separates data from address
             address = inboundData[1]                      # Separates address from data
@@ -129,14 +124,14 @@ class GameServer(Thread):
                 self.clientScreenDimensions[str(address)] = dimensions
             else:
                 if data['command'] != "":
-                    print(data, file=self.logs)   # Only prints out non-trivial data from clients                            
+                    print("("+self.HOST, ",", self.PORT +")::", data, file=self.logs)   # Only prints out non-trivial data from clients                            
                     
                     # Handle commands from other servers
                     if data['command'] == "close":              # Ends the server
                         break
 
                     if data['command'] == 'ping':               # Confirms connection to client servers
-                        print(self.clients, file=self.logs)
+                        print("("+self.HOST, ",", self.PORT +")::", self.clients, file=self.logs)
                 
                     if data['command'] == 'start':
                         for client in self.clients:             # Tells both player views to move on
@@ -149,13 +144,17 @@ class GameServer(Thread):
             outboundData = pickle.dumps(gameState)
             self.socket.sendto(outboundData, address)
 
+            # Continuously saves logging information to a text file:
+            self.logs.close()
+            self.logs = open(str(self.filepath)+"/_logs/"+ str(self.PORT) + ".txt", "a+")
+
             # Check client connections here
             # self.cleanClientList(time.time())
 
     def setupStage(self):
         """Determines dimensions of the game board, players' names, and eventually the player's army.
         Return a tuple containing (Board, Player1, Player2)."""
-        print("--- Initiating setup stage", file=self.logs)
+        print("("+self.HOST, ",", self.PORT +"):: Initiating setup stage", file=self.logs)
 
         # Holds player map and name information here. Will be used to create objects later.
         mapVotes = []
@@ -230,7 +229,7 @@ class GameServer(Thread):
         """Communicates with player objects.
            Controls player turn-taking for placement.
            Keeps track of board and player changes."""
-        print("--- Entering placement stage", file=self.logs)
+        print("("+self.HOST, ",", self.PORT +"):: Entering placement stage", file=self.logs)
 
         activePlayer = self.players[0]
         readyPlayers = set()
@@ -295,7 +294,7 @@ class GameServer(Thread):
         self.logs.close()
         self.logs = open(str(self.filepath)+"/_logs/"+ str(self.PORT) + ".txt", "a+")
 
-        print("-- Entering battle stage", file=self.logs)
+        print("("+self.HOST, ",", self.PORT +"):: Entering battle stage", file=self.logs)
 
         # Prepare the board for play; removes red and blue squares from the board
         self.game.normalizeBoard()
